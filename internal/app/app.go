3c9725ca7bdf624f89103bc3dfa66ea98a8b32c6
cestty <vlad-feoktistov-2017@mail.ru>
2026-02-27 12:07:02 +0300

    Add fast tool bootstrap and background auto-update

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,20 +1,27 @@
 package app
 
 import (
-	fyneapp "fyne.io/fyne/v2/app"
+	"context"
+	"time"
 
 	"YtDownloader/internal/bundled"
 	"YtDownloader/internal/ui"
 	"YtDownloader/internal/ytdlp"
+
+	fyneapp "fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/dialog"
 )
 
 func Run() {
 	a := fyneapp.NewWithID("com.cessttyy.ytDownloader")
 	applyEmbeddedFont(a)
 
-	tools, err := bundled.EnsureTools("YtDownloader", true)
+	tools, err := bundled.EnsureToolsFast("YtDownloader", true)
 	if err != nil {
-		panic(err)
+		w := a.NewWindow("YtDownloader")
+		dialog.ShowError(err, w)
+		w.ShowAndRun()
+		return
 	}
 
 	cli := &ytdlp.Runner{
@@ -23,5 +30,15 @@ func Run() {
 	}
 
 	w := ui.ShowMainWindow(a, cli)
+
+	go func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 45*time.Second)
+		defer cancel()
+
+		if _, err := bundled.EnsureToolsAutoUpdate(ctx, "YtDownloader", true); err != nil {
+			return
+		}
+	}()
+
 	w.ShowAndRun()
 }
